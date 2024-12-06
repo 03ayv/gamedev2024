f09bc815f55bb0e5ae538fe8ec5854fee6e7f369
anna <03ayv21@gmail.com>
2024-12-06 16:02:30 +0100

    refactor animation

--- project/LeshyLeaf.cs
+++ project/LeshyLeaf.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using project.Animations;
 using project.Interfaces;
 using SharpDX.Direct3D9;
 using System;
@@ -13,25 +14,30 @@ namespace project
     public class LeshyLeaf: IGameObject
     {
         Texture2D leshyLeafTexture;
-        private Rectangle deelRectangle;
-        private int schuifOp_X = 0;
+        Animation animation;
         public LeshyLeaf(Texture2D texture)
         {
             leshyLeafTexture = texture;
-            deelRectangle = new Rectangle(schuifOp_X, 32, 32, 32);
+            animation = new Animation();
+            animation.AddFrame(new AnimationFrame(new Rectangle(0,32,32,32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(32,32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(64, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(96, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(128, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(160, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(192, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(224, 32, 32, 32)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(256, 32, 32, 32)));
         }
 
         public void Update()
         {
-            schuifOp_X += 32;
-            if (schuifOp_X > 256)
-                schuifOp_X = 0;
-            deelRectangle.X = schuifOp_X;
+            animation.Update();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(leshyLeafTexture, new Vector2(10, 10), deelRectangle, Color.White);
+            spriteBatch.Draw(leshyLeafTexture, new Vector2(10, 10), animation.CurrentFrame.SourceRectangle, Color.White);
         }
     }
 }
--- project/obj/Debug/net6.0-windows/project.AssemblyInfo.cs
+++ project/obj/Debug/net6.0-windows/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2ebd1404a296fbfa37e55f00e310fce001b772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80dd4af9fcbc445407719f9da72039070a141e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
